261bb8c258a2d271a99f4e81eb16c2918f1e46d0
Denis Ivanchenko <sicknature24@yandex.com>
2021-06-12 14:27:43 +0300

    add store functionality with reflection

---
+++ consoleApp/src/main/java/com/denis/consoleapp/StoreApp.java
@@ -0,0 +1,11 @@
+package com.denis.consoleapp;
+
+import com.denis.store.Store;
+
+public class StoreApp {
+
+    public static void main(String[] args) {
+        Store store = new Store();
+        store.print();
+    }
+}
